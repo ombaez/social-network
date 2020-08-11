773e45cd15b5515891b69b64bc65d436bd5d9499
ombaez <ombaez5689@gmail.com>
2020-08-10 23:33:16 -0300

    feat: add navbar and landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Navbar from './components/layout/Navbar'
+import Landing from './components/layout/Landing'
 import './App.css';
 
 const App = () => {
   return (<>
-
-    Ghost
+    <Navbar />
+    <Landing />
   </>
   );
 }
