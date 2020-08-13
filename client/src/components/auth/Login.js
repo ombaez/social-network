d98ef85deb45d74c187e5cf669ca19739d26f742
ombaez <ombaez5689@gmail.com>
2020-08-12 23:45:36 -0300

    feat: add Login and Register UI

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
-const Login = () => {
+export const Login = () => {
+    const [formData, setFormData] = useState({
+        email:'',
+        password:'',
+        password2:'',
+    })
+
+    const {email,password,password2} = formData
+    const onChange = e => setFormData({...formData,[e.target.name]:e.target.value})
+    const onSubmit = async(e) => {
+        e.preventDefault()
+            console.log('SUCCESS')
+        }
+    
     return (
-        <div>
-            Login
-        </div>
+        <>
+            <h1 className="large text-primary">Sign In</h1>
+            <p className="lead"><i className="fas fa-user"></i> Sign Into Your Account</p>
+            <form className="form" onSubmit={(e)=>onSubmit(e)}>
+                <div className="form-group">
+                    <input type="email" 
+                    onChange={(e)=>onChange(e)} required
+                        value={email}
+                    placeholder="Email Address" name="email" />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        minLength="6"
+                        onChange={(e)=>onChange(e)}
+                        value={password}
+                    />
+                </div>
+                <input type="submit" className="btn btn-primary" value="Login" />
+            </form>
+            <p className="my-1">
+                Don't have an account? <Link to="/register">Sign Up</Link>
+
+            </p>
+        </>
     )
 }
 
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,18 +1,41 @@
 import React, { useState } from 'react'
-
+import { Link } from 'react-router-dom'
 
 export const Register = () => {
-    const [formData, setformData] = useState()
+    const [formData, setFormData] = useState({
+        name:'',
+        email:'',
+        password:'',
+        password2:'',
+    })
+
+    const {name,email,password,password2} = formData
+    const onChange = e => setFormData({...formData,[e.target.name]:e.target.value})
+    const onSubmit = async(e) =>{e.preventDefault()
+        if(password !== password2){
+            console.log('Password not match')
+        } else {
+            console.log('SUCCESS')
+            }
+        }
+    
     return (
         <>
             <h1 className="large text-primary">Sign Up</h1>
             <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
-            <form className="form" action="create-profile.html">
+            <form className="form" onSubmit={(e)=>onSubmit(e)}>
                 <div className="form-group">
-                    <input type="text" placeholder="Name" name="name" required />
+                    <input type="text" 
+                        onChange={(e)=>onChange(e)}
+                        placeholder="Name" 
+                        value={name}
+                    name="name" required />
                 </div>
                 <div className="form-group">
-                    <input type="email" placeholder="Email Address" name="email" />
+                    <input type="email" 
+                    onChange={(e)=>onChange(e)} required
+                        value={email}
+                    placeholder="Email Address" name="email" />
                     <small className="form-text"
                     >This site uses Gravatar so if you want a profile image, use a
             Gravatar email</small
@@ -24,6 +47,8 @@ export const Register = () => {
                         placeholder="Password"
                         name="password"
                         minLength="6"
+                        onChange={(e)=>onChange(e)}
+                        value={password}
                     />
                 </div>
                 <div className="form-group">
@@ -31,13 +56,15 @@ export const Register = () => {
                         type="password"
                         placeholder="Confirm Password"
                         name="password2"
+                        onChange={(e)=>onChange(e)}
                         minLength="6"
+                        value={password2}
                     />
                 </div>
                 <input type="submit" className="btn btn-primary" value="Register" />
             </form>
             <p className="my-1">
-                Already have an account? <a href="login.html">Sign In</a>
+                Already have an account? <Link to="/login">Sign In</Link>
             </p>
         </>
     )
