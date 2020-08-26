59ea61cc181c9cb8a921bd56a50537249ac00071
ombaez <ombaez5689@gmail.com>
2020-08-26 01:04:57 -0300

    feat: re check private routes works

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -5,7 +5,8 @@ import {
   AUTH_ERROR,
   USER_LOADED,
   LOGIN_SUCCESS,
-  LOGIN_FAIL
+  LOGIN_FAIL,
+  LOGOUT
 } from "./types";
 import { setAlert } from "./alert";
 import setAuthToken from "../utils/setAuthToken";
@@ -90,3 +91,10 @@ export const login = (email, password) => async dispatch => {
     });
   }
 };
+
+
+// LOGOUT / CLear profile
+
+export const logout = () => dispatch => {
+  dispatch({ type: LOGOUT })
+}
